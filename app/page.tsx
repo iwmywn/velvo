03751776ff9e65f744309b1918bc06d4476c64c8
Anh Tuan <thongocnguyen1011@gmail.com>
2024-11-23 09:55:59 +0700

    no need Image tag now

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import categories from "./data/categories";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,11 +11,10 @@ export default function Home() {
           className={`flex w-full flex-col rounded border border-black/5 bg-stone-100 py-4 md:flex-row ${index % 2 !== 0 && "md:flex-row-reverse"}`}
         >
           <div className="flex max-h-[25rem] justify-center px-3 md:w-[50%]">
-            <Image
+            <img
               className="max-h-full max-w-full object-contain"
               src={src}
               alt={alt}
-              loading="lazy"
             />
           </div>
           <div className="flex flex-col items-center justify-center gap-4 px-4 text-center md:w-[50%]">
--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Logo() {
   return (
     <Link className="h-7 w-7 md:h-8 md:w-8" href="/">
-      <Image className="h-full w-full" src="/logo.png" alt="logo" />
+      <img className="h-full w-full" src="/logo.png" alt="logo" />
     </Link>
   );
 }
