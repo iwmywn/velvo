cb28889bb4b665b46d635fd0f0859c846f71a5ec
Anh Tuan <thongocnguyen1011@gmail.com>
2024-11-25 17:17:27 +0700

    preload image

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -4,7 +4,13 @@ import Image from "next/image";
 export default function Logo() {
   return (
     <Link className="relative h-7 w-7 md:h-8 md:w-8" href="/">
-      <Image src="/logo.png" alt="logo" fill style={{ objectFit: "contain" }} />
+      <Image
+        src="/logo.png"
+        alt="logo"
+        fill
+        priority
+        style={{ objectFit: "contain" }}
+      />
     </Link>
   );
 }
