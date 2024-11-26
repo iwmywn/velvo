32f2d1a92525ba1abb923822432f5ed8fd0d5723
Anh Tuan <thongocnguyen1011@gmail.com>
2024-11-26 14:02:34 +0700

    fix css (cart)

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -27,10 +27,10 @@ export default function CartPage() {
     <main className="relative z-10 bg-white px-8 pb-28 pt-10 md:px-20">
       <h3 className="mb-5 text-xl font-medium">REVIEW YOUR ORDER (mock)</h3>
       <div className="flex flex-col overflow-x-scroll text-sm min-[400px]:overflow-visible">
-        <div className="mb-4 grid grid-cols-6 rounded-lg border sm:grid-cols-7">
+        <div className="mb-4 grid grid-cols-5 rounded-lg border sm:grid-cols-6 lg:grid-cols-7">
           {heads.map((head, index) => (
             <div
-              className={`flex items-center justify-center text-nowrap py-2 font-medium ${index === 0 && "col-span-2 sm:col-span-3"}`}
+              className={`flex items-center justify-center text-nowrap py-2 font-medium ${index === 0 && "col-auto sm:col-span-2 lg:col-span-3"}`}
               key={head}
             >
               {head}
@@ -39,10 +39,10 @@ export default function CartPage() {
         </div>
         {mockProduct.map(({ href, name, price, quantity }, index) => (
           <div
-            className="mb-2 grid grid-cols-6 rounded-lg border py-2 sm:grid-cols-7 md:py-0"
+            className="mb-2 grid grid-cols-5 rounded-lg border py-2 sm:grid-cols-6 md:py-0 lg:grid-cols-7"
             key={index}
           >
-            <div className="relative col-span-2 flex flex-col items-center justify-center gap-x-3 gap-y-1 p-1 text-center sm:col-span-3 md:flex-row md:justify-normal md:text-left">
+            <div className="relative col-auto flex flex-col items-center justify-center gap-x-3 gap-y-1 p-1 text-center sm:col-span-2 md:flex-row md:justify-normal md:text-left lg:col-span-3">
               <span className="relative h-24 w-24 flex-shrink-0">
                 <Image
                   src={href}
