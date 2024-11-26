57323e8763d5926933aa49ecc173fad307343f02
Anh Tuan <thongocnguyen1011@gmail.com>
2024-11-26 13:56:00 +0700

    fix css (cart)

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -24,7 +24,7 @@ const centerClass = "flex items-center justify-center text-center";
 
 export default function CartPage() {
   return (
-    <main className="relative z-10 px-8 pt-4 md:px-20">
+    <main className="relative z-10 bg-white px-8 pb-28 pt-10 md:px-20">
       <h3 className="mb-5 text-xl font-medium">REVIEW YOUR ORDER (mock)</h3>
       <div className="flex flex-col overflow-x-scroll text-sm min-[400px]:overflow-visible">
         <div className="mb-4 grid grid-cols-6 rounded-lg border sm:grid-cols-7">
