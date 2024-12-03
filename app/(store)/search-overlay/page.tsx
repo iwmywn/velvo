372ecc95d0e4cf17c9e91a666f69495be12b4f54
Anh Tuan <thongocnguyen1011@gmail.com>
2024-12-03 12:14:52 +0700

    redirect to home

--- app/(store)/search-overlay/page.tsx
+++ app/(store)/search-overlay/page.tsx
@@ -1,18 +1,5 @@
-"use client";
+import { redirect } from "next/navigation";
 
-import { useEffect, useState } from "react";
-import Loading from "@/ui/loading";
-
-export default function RedirectToPreviousPage() {
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    window.history.back();
-    const timeoutId = setTimeout(() => {
-      setLoading(false);
-    }, 200);
-    return () => clearTimeout(timeoutId);
-  }, []);
-
-  return loading ? <Loading /> : null;
+export default function RedirectToPurchase() {
+  redirect("/");
 }
