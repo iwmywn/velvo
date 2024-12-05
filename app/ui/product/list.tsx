5254378a8a5770037a5880ca272437197e2c6160
Anh Tuan <thongocnguyen1011@gmail.com>
2024-12-05 17:07:51 +0700

    fix grid layout

--- app/ui/product/list.tsx
+++ app/ui/product/list.tsx
@@ -29,7 +29,7 @@ export default function ProductList({
       <h3 className="mb-7 mt-5 text-center text-xl font-bold uppercase">
         {title}
       </h3>
-      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 min-[2000px]:grid-cols-5 min-[3000px]:grid-cols-6">
+      <div className="grid grid-cols-1 gap-6 min-[600px]:grid-cols-2 min-[1000px]:grid-cols-3 min-[1400px]:grid-cols-4 min-[1900px]:grid-cols-5 min-[2300px]:grid-cols-6 min-[2700px]:grid-cols-7">
         {products.map((product) => (
           <ProductCard key={product.slug} {...product} />
         ))}
