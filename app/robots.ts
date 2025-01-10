ad9af9fafcf8469f7ed98e3e6c854edd9fcec6fe
Anh Tuan <thongocnguyen1011@gmail.com>
2025-01-10 21:51:11 +0700

    fix sitemap url in robots file

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "/sitemap.xml",
+    sitemap: `${process.env.NEXT_PUBLIC_URL}/sitemap.xml`,
   };
 }
