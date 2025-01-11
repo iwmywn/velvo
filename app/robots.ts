f6805d3d967aa5ab5aa14afd7b27c95fb9e7c4b1
Anh Tuan <thongocnguyen1011@gmail.com>
2025-01-11 13:53:42 +0700

    fix robots file

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: "/",
+      disallow: ["/user/*", "/email-handler"],
     },
     sitemap: `${process.env.NEXT_PUBLIC_URL}/sitemap.xml`,
   };
