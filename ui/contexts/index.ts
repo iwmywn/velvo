4a6ecd8d818372dd2d63432178415703b1addf8b
Anh Tuan <thongocnguyen1011@gmail.com>
2025-02-21 11:20:41 +0700

    rename product to store (context file)

--- ui/contexts/index.ts
+++ ui/contexts/index.ts
@@ -1,4 +1,4 @@
 export { AuthProvider, useAuthContext } from "./auth";
 export { HeightProvider, useHeightContext } from "./height";
-export { StoreProvider, useStoreContext } from "./product";
+export { StoreProvider, useStoreContext } from "./store";
 export { UIStateProvider, useUIStateContext } from "./state";
